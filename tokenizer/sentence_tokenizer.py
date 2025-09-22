401b5473f36a10f0b5a1f81bdfc1b70a1e89704a
Stark <dp397113@gmail.com>
2025-09-23 02:06:04 +0530

    style: removed self from agruments

--- tokenizer/sentence_tokenizer.py
+++ tokenizer/sentence_tokenizer.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 
 
                 
-def split_sentences(self, response: str) -> List[Tuple[int, str]]:
+def split_sentences(response: str) -> List[Tuple[int, str]]:
     sentences = text_to_sentences(response).split("\n")
     clean_sentences = [s.strip() for s in sentences if s.strip()]
     return list(enumerate(clean_sentences))
